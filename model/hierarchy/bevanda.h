d930f6c3f9b08ba4ac964be677c8dbbdf9424b90
Luca Zaninotto <lucazanny@gmail.com>
2020-03-23 16:10:20 +0100

    Espanso bevanda? penso si dica così

--- model/hierarchy/bevanda.h
+++ model/hierarchy/bevanda.h
@@ -1,8 +1,15 @@
 #ifndef BEVANDA_H
 #define BEVANDA_H
+#include "bar.h"
+#include <string>
 
-class Bevanda: public Bar{
+class Bevanda {
+protected:
+	double prezzo;
+	std::string nome_generico;
 public:
+	Bevanda(const std::string& nome, double prezzo);
+	virtual ~Bevanda() = default;
 	
 };
 
