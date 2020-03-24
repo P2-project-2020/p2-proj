45a0437cd75d8bf3d047686b7f3916b2fe059e66
Erik Nucibella <eriknucib@live.it>
2020-03-24 22:55:50 +0100

    Almost definitive proj structure

---
+++ view/filterExample.h
@@ -0,0 +1,6 @@
+//Qt MVC is based on 2 widgets: QListview & QtableView
+// They both provide real time search, edit, ordering and visualization over da$
+/*Filter is the class used for different purpouse: editing,deleting and sortin
+are just examples of what this allows to do*/
+
+//Qt provide good but not so easy to learn docs for this */
---
+++ view/insertExample.h
@@ -0,0 +1,1 @@
+//This should be a mask for the insertion of everything in our app 
