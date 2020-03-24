44f306d6211ab8b8214c30278a7f6768f57c1f3f
Luca Zaninotto <lucazanny@gmail.com>
2020-03-24 09:53:53 +0100

    commentato meglio

--- model/hierarchy/bevanda.h
+++ model/hierarchy/bevanda.h
@@ -18,6 +18,8 @@ public:
 
 	/* Ha anchesenso per i coctail dove la parte di alcol dipende da quanto alcolico c'è*/
 	virtual double percentualeAlcol() const = 0;
+	/* Questo lo possiamo implementare già qui in modo che guardi il metodo sopra e decida
+	   rispetto la soglia */
 	bool isSuperalcolico() const;
 };
 
