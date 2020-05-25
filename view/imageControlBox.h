28f5215c231b2e0d40d81b5cfa73741c870744dc
badadumTss <lucazanny@gmail.com>
2020-05-26 00:11:06 +0200

    Aggiunto entry-point per imageControlBox

--- view/imageControlBox.h
+++ view/imageControlBox.h
@@ -8,6 +8,21 @@
 #include "toolboxrow.h"
 #include "imageView.h"
 
+/**
+  
+  Questo è il widget principale, si occupa di chiamare ImageView e roolboxRow che invece gesticono 
+  rispettivamente l'immagine e i controlli relativi.
+  Può essere creato direttamente passandogli la path (assoluta) dell'immagine da mostrare.
+  i metodi sono più o meno auto-esplicativi.
+  
+  - toolboxRow ::  si occupa di mostrare i dialoghi per settare l'immagine, ed emette (quando scelta) la path
+  per la nuova immagine scelta, rimbalzata tramite `signal_setImage(const QString&)'.
+  
+  -  `slot_removeImage()' si occupa di rimuovere la preview, serve a notificare il controller che l'associazione
+  oggetto-immagine va rotta. Va connesso con connect.
+
+*/
+
 class ImageControlBox : public QWidget{
 	Q_OBJECT;
 
