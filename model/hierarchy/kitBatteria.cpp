242da4f08c4818097dc424f452874fc201481991
badadumTss <lucazanny@gmail.com>
2020-06-10 00:06:30 +0200

    Kit batteria messo

--- model/hierarchy/kitBatteria.cpp
+++ model/hierarchy/kitBatteria.cpp
@@ -1,24 +1,29 @@
 #include "kitBatteria.h"
-const std::map<KitBatteria::Material,std::string> KitBatteria::materials = {{KitBatteria::woodKit, "legno"},
-																			{KitBatteria::plexiglassKit, "plexiglas"}};
+const std::vector<std::string> KitBatteria::materials = {"legno", "plexiglas"};
 const QString KitBatteria::json_material = "materiale";
 const QString KitBatteria::json_snare = "rullante in metallo";
 
-KitBatteria::KitBatteria(double _price, const std::string& _brand, KitBatteria::Material _material, bool _metalSnare, bool _used, const std::string& _desc):
-	Strumento(_price, _brand, _used, _desc), Percussione(false), material(_material), metalSnare(_metalSnare){}
+KitBatteria::KitBatteria(double _price, const std::string& _brand, int _material, bool _metalSnare, bool _used, const std::string& _desc):
+     Strumento(_price, _brand, _used, _desc), Percussione(false), material(_material), metalSnare(_metalSnare){}
 
 std::string KitBatteria::className() const {
-	return std::string("Batteria ") + (metalSnare ? "con rullante in metallo" : "");
+     return std::string("Batteria ") + (metalSnare ? "con rullante in metallo" : "");
 }
 
 std::string KitBatteria::getMaterial() const {
-	return (material == woodKit ? "Legno" : "Plexiglas");
+     return materials[material];
 }
 
-KitBatteria::Material KitBatteria::findMaterial(const std::string& str){
-	for(const auto& it : materials)
-		if(it.second == str) return it.first;
-	return materials.begin()->first;
+void KitBatteria::setMaterial(int new_material){
+     if(new_material >= 0 && new_material < materials.size())
+	  material = new_material;
+}
+
+int KitBatteria::findMaterial(const std::string& str){
+     for(int i=0; i < materials.size(); ++i){
+	  if(str == materials.at(i)) return i;
+     }
+     return -1;
 }
 
 // std::string KitBatteria::materialToString(const KitBatteria::Material& _material){
@@ -26,20 +31,20 @@ KitBatteria::Material KitBatteria::findMaterial(const std::string& str){
 // }
 
 void KitBatteria::loadData(const QJsonObject& obj){
-	Percussione::loadData(obj);
+     Percussione::loadData(obj);
 
-	const QJsonValue& valMaterial = obj[json_material];
-	const QJsonValue& valSnare = obj[json_snare];
+     const QJsonValue& valMaterial = obj[json_material];
+     const QJsonValue& valSnare = obj[json_snare];
 
-	if(!valMaterial.isUndefined() && valMaterial.isString())
-		material = KitBatteria::findMaterial(valMaterial.toString().toStdString());
-	if(!valSnare.isUndefined() && valSnare.isBool())
-		metalSnare = valSnare.toBool();
+     if(!valMaterial.isUndefined() && valMaterial.isString())
+	  material = KitBatteria::findMaterial(valMaterial.toString().toStdString());
+     if(!valSnare.isUndefined() && valSnare.isBool())
+	  metalSnare = valSnare.toBool();
 }
 
 void KitBatteria::saveData(QJsonObject& obj) const {
-	Percussione::saveData(obj);
+     Percussione::saveData(obj);
 
-	obj[json_material] = QString::fromStdString(KitBatteria::materials.at(material));
-	obj[json_snare] = metalSnare;
+     obj[json_material] = QString::fromStdString(KitBatteria::materials.at(material));
+     obj[json_snare] = metalSnare;
 }
--- model/hierarchy/kitBatteria.h
+++ model/hierarchy/kitBatteria.h
@@ -3,24 +3,24 @@
 
 class KitBatteria: public Percussione{
 public:
-	enum Material {woodKit, plexiglassKit};
-	static const std::map<KitBatteria::Material,std::string> materials;
+     static const std::vector<std::string> materials;
 	
-	KitBatteria(double = 0, const std::string& = "", KitBatteria::Material = woodKit, bool = false, bool = false, const std::string& = "");
+     KitBatteria(double = 0, const std::string& = "", int = 0, bool = false, bool = false, const std::string& = "");
 
-	std::string className() const;
-	Strumento::Tune tune() const { return undefined; }
-	std::string getMaterial() const;
+     std::string className() const;
+     Strumento::Tune tune() const { return undefined; }
+     std::string getMaterial() const;
+     void setMaterial(int);
 
-	void loadData(const QJsonObject&);
-	void saveData(QJsonObject&) const;
+     void loadData(const QJsonObject&);
+     void saveData(QJsonObject&) const;
+
+     static int findMaterial(const std::string&);
+     
 private:
-	Material material;
-	bool metalSnare;
-
-	static KitBatteria::Material findMaterial(const std::string&);
-	static std::string materialToString(const KitBatteria::Material&);
+     int material;
+     bool metalSnare;
 	
-	static const QString json_material;
-	static const QString json_snare;
+     static const QString json_material;
+     static const QString json_snare;
 };
