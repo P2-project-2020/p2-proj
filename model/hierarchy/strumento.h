82051ae656f3785f5218533e5de3de0cef43b309
badadumTss <lucazanny@gmail.com>
2020-06-17 23:58:39 +0200

    tuneToString era proprio inutile

--- model/hierarchy/strumento.h
+++ model/hierarchy/strumento.h
@@ -13,8 +13,6 @@ class Strumento{
 public:
      enum Tune {undefined, soprano, contralto, tenor, baritone, bass};
      static const std::map<Strumento::Tune, std::string> Tunes;
-	
-     static std::string tuneToString(const Strumento::Tune&); //USELESS
 
      static const std::string lutherie; // serve come stringa reference in più punti del codice
      static const QString json_type; // QString sulla quale fare switch nella gestione del documento
