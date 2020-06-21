cd37b9bf1f2315d29244be2604434b79277d76a7
badadumTss <lucazanny@gmail.com>
2020-06-21 16:56:01 +0200

    cambiato file dialog in toolboxRox

--- view/toolboxrow.cpp
+++ view/toolboxrow.cpp
@@ -3,84 +3,78 @@
 #include <iostream>
 
 toolboxRow::toolboxRow(QWidget* parent):
-	QWidget(parent),
-	load(new QPushButton(QIcon::fromTheme("folder"),"Apri",this)),
-	remove(new QPushButton(QIcon::fromTheme("process-stop"),"Elimina",this)),
-	refresh(new QPushButton(QIcon::fromTheme("view-refresh"),"Refresh",this)),
-	layout(new QHBoxLayout(this))
+     QWidget(parent),
+     load(new QPushButton(QIcon::fromTheme("folder"),"Apri",this)),
+     remove(new QPushButton(QIcon::fromTheme("process-stop"),"Elimina",this)),
+     refresh(new QPushButton(QIcon::fromTheme("view-refresh"),"Refresh",this)),
+     layout(new QHBoxLayout(this))
 {
-	layout->addStretch();
-	layout->addWidget(load);
-	layout->addWidget(remove);
-	layout->addWidget(refresh);
-	// layout->addStretch(); // Se si vuole avere i pulsanti al centro o a sinistra
+     layout->addStretch();
+     layout->addWidget(load);
+     layout->addWidget(remove);
+     layout->addWidget(refresh);
+     // layout->addStretch(); // Se si vuole avere i pulsanti al centro o a sinistra
 
-	setLayout(layout);
+     setLayout(layout);
 	
-	QObject::connect(load, SIGNAL(clicked()), this, SLOT(slot_load()));
-	QObject::connect(remove, SIGNAL(clicked()), this, SLOT(slot_remove()));
-	QObject::connect(refresh, SIGNAL(clicked()), this, SLOT(slot_refresh()));
+     QObject::connect(load, SIGNAL(clicked()), this, SLOT(slot_load()));
+     QObject::connect(remove, SIGNAL(clicked()), this, SLOT(slot_remove()));
+     QObject::connect(refresh, SIGNAL(clicked()), this, SLOT(slot_refresh()));
 }
 
 void toolboxRow::initFileDialog(QFileDialog &dialog)
 {
-	// Chiede al sistema la path delle immaigni, se non la trova apre quella in cui è l'eseguibile
-	const QStringList picturesLocations = QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
-	dialog.setDirectory(picturesLocations.isEmpty() ? QDir::currentPath() : picturesLocations.last());
+     // Chiede al sistema la path delle immaigni, se non la trova apre quella in cui è l'eseguibile
+     const QStringList picturesLocations = QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
+     dialog.setDirectory(picturesLocations.isEmpty() ? QDir::currentPath() : picturesLocations.last());
 
-    QStringList mimeTypeFilters;
-    const QByteArrayList supportedMimeTypes = QImageReader::supportedMimeTypes();
+     QStringList mimeTypeFilters;
+     const QByteArrayList supportedMimeTypes = QImageReader::supportedMimeTypes();
 
-	for (const QByteArray &mimeTypeName : supportedMimeTypes)
-        mimeTypeFilters.append(mimeTypeName);
+     for (const QByteArray &mimeTypeName : supportedMimeTypes)
+	  mimeTypeFilters.append(mimeTypeName);
 
-	mimeTypeFilters.sort();
+     mimeTypeFilters.sort();
 	
-	dialog.setMimeTypeFilters(mimeTypeFilters);
-	dialog.selectMimeTypeFilter("image/jpg");
-	/* In pratica setta il file explorer in modo che apra /solo/ le immagini */
+     dialog.setMimeTypeFilters(mimeTypeFilters);
+     dialog.selectMimeTypeFilter("image/jpg");
+     /* In pratica setta il file explorer in modo che apra /solo/ le immagini */
 }
 
 void toolboxRow::slot_load(){
 
-    //filter= "JPG (*.jpg);; PNG(*.png)"
-    /*
-    QFileDialog *fileDialog = new QFileDialog;
-    fileDialog->setDefaultSuffix("pdf");
-    filename=
-            fileDialog->getSaveFileName(
-                 this,
-                 tr("Scegli dove salvare il file"),
-                 QDir::currentPath(),
-                 filter,&filter,QFileDialog::DontUseNativeDialog);
-    //Se l'utente non ha inserito l'estensione la aggiungo
+     QString filter= "JPG (*.jpg);; PNG(*.png)";
+    
+     QFileDialog fileDialog;
 
-    */
-	QFileDialog dialog(this, tr("Open File"));
-
-	initFileDialog(dialog);
-
-	bool imgAccepted = false;
-	QString filename;
-	while(!imgAccepted && dialog.exec() == QDialog::Accepted)
-	{
-		filename = dialog.selectedFiles().first();
-		QImage imgReader(QString("file:///").append(filename));
-		imgAccepted = imgReader.isNull();
-		if(!imgAccepted){
-			QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
-									 tr("Impossibile caricare %1")
-									 .arg(QDir::toNativeSeparators(filename)));
-		}
-	}
-	if(imgAccepted)
-		emit signal_load(filename);
+     bool imgAccepted = false;
+     QString filename = "";
+     while(!imgAccepted && fileDialog.acceptMode() == QFileDialog::AcceptOpen)
+     {
+	  filename = fileDialog.getOpenFileName(
+	       this,
+	       tr("Scegli Il file da caricare"),
+	       QDir::currentPath(),
+	       filter,
+	       &filter,
+	       QFileDialog::DontUseNativeDialog);
+	     
+	  QImage imgReader(QString("file:///").append(filename));
+	  imgAccepted = imgReader.isNull();
+	  if(!imgAccepted){
+	       QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+					tr("Impossibile caricare %1")
+					.arg(QDir::toNativeSeparators(filename)));
+	  }
+     }
+     if(imgAccepted)
+	  emit signal_load(filename);
 }
 
 void toolboxRow::slot_remove(){
-	emit signal_remove();
+     emit signal_remove();
 }
 
 void toolboxRow::slot_refresh(){
-	emit signal_refresh();
+     emit signal_refresh();
 }
